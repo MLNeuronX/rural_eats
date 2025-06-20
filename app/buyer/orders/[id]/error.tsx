ceca86eafd49e1fc1bae6f7e53e1e7f9f0c4619c
Ali Raza <ar1914402@gmail.com>
2025-06-20 14:22:31 +0500

    Fix: add valid error boundary component for order error page

--- app/buyer/orders/[id]/error.tsx
+++ app/buyer/orders/[id]/error.tsx
@@ -1,1 +1,14 @@
-import { ThemeProvider } from "@/components/theme-provider"; 
+'use client';
+
+import { ThemeProvider } from "@/components/theme-provider";
+
+export default function Error() {
+  return (
+    <ThemeProvider>
+      <div style={{ padding: 32, textAlign: 'center' }}>
+        <h1>Something went wrong</h1>
+        <p>Please try again or contact support.</p>
+      </div>
+    </ThemeProvider>
+  );
+} 
